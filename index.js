bb59ed6cc01d3a5c3d49cc7cf23cc799665f800c
Javi <javi@altabert.com>
2021-11-11 22:59:01 +0100

    Add js scripts

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const name = 'Martin';
 console.log(`Hello ${name}`);
+
+var $;
+$ = require('jquery');
+
+$(function() {
+
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+
+        if (scrollPos >= 500) {
+            $('#btnTop').fadeIn();
+        } else {
+            $('#btnTop').fadeOut();
+        }
+    });
+
+    $('#btnTop').on('click', function(){
+        subirScroll();
+    });
+
+    function subirScroll() {
+        $(window).scrollTop(0);
+    }
+});
